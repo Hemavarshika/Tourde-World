408ec6948f8c3a42b0259a8cb3e2692220013b8a
Hemavarshika <82502920+Hemavarshika@users.noreply.github.com>
2022-06-06 23:15:14 +0530

    added

--- my-app/src/components/pages/Services.js
+++ my-app/src/components/pages/Services.js
@@ -1,7 +1,41 @@
-import React from 'react'
-import'../../App.css'
+import React, {useState,useEffect} from 'react';
+import'../../App.css';
+import axios from 'axios';
 
-export default function Services()
-{
-    return <h1 className='services'>SERVICES</h1>;
+function Services(){
+    const[data,setData] = useState(null);
+    //const [myArray, datas] = useState([]);
+    const [datas, setDatas] = useState(null); 
+    async function fetchData(xid){
+        try{
+            const res = await axios.get('https://api.opentripmap.com/0.1/en/places/xid/'+xid+'?apikey=5ae2e3f221c38a28845f05b61cf9ff614d3e7ce7d8c01eb2ff7f49b6');
+            setDatas(datas => [datas,res.data]);
+            setData(res.data);
+            console.log(datas);
+            console.log(res)
+            console.log(data.xid);
+        }catch(err){
+            console.log(err);
+        }
+
+    }
+    useEffect(()=>{
+        const Variables=['N3309149440','R4872552','R3721155','W35107548'];
+       // const n= Variable.length;
+        for (const [i, Variable] of Variables.entries()) {
+            fetchData(Variable);
+          }
+      
+    }, []);
+    return(
+        <>
+           
+               {d && <img src={d.preview.source} alt='Test'/>} 
+             
+        </>
+    );
+
 }
+
+export default Services;
+
